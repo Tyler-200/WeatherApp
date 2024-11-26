d2687ec7d2c8ec2c7d94b27bb1e84f860fc88f33
Tyler-200 <hughestyler2000@gmail.com>
2024-11-26 10:22:38 +0000

    Added API call to openWeather added key interface items minmal styling added

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 /** weather application JavaScript
  *  Author: Tyler Hughes
  */
+const apiKey = "91a1c2e2eb9a1b4e7d9016d0a5d6a0be"
+const url = "https://api.openweathermap.org/data/2.5/weather?q="
+
+//https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+
+async function checkWeather(place){
+    var search = url + place + "&appid=" + apiKey;
+    console.log(search)
+    const res = await fetch(search);
+    var data = await res.json();
+    console.log(data);
+}
+checkWeather("prestatyn");
